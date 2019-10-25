c69d1ae7ab4af58ead97a98a17dd7cd1400fd030
davidsalsa <davidsalsbach@hotmail.com>
2019-10-25 13:58:51 +0200

    Added scripts and scene files new branch

---
+++ Scripts/AttackState.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackState : IState
+{
+    private GameObject turret;
+    private GameObject target;
+    private TankData tankData;
+    private Transform bulletSpawnPoint;
+    private GameObject bullet;
+    private float bulletSpeed = 1500f;
+    StateController stateController;
+
+
+    public AttackState(GameObject turret, GameObject enemy, Transform bulletSpawnPoint, GameObject bullet, StateController stateController)
+    {
+        this.turret = turret;
+        this.target = enemy;
+        this.bulletSpawnPoint = bulletSpawnPoint;
+        this.bullet = bullet;
+        this.stateController = stateController;
+        
+        tankData = ScriptableObject.CreateInstance<TankData>();
+    }
+
+    public void doAction()
+    {
+        Vector3 direction = (target.transform.position - turret.transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Debug.Log(turret);
+        turret.transform.rotation = Quaternion.Slerp(
+            turret.transform.rotation, lookRotation, 
+            tankData.maxRotationSpeed * Time.deltaTime);
+
+        shoot();
+        deleteBulletInstance();
+    }
+
+    public void Transition()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void shoot()
+    {
+        if (lockedOn())
+        {
+            // bullet needs to be instantiated
+            bullet.transform.position = bulletSpawnPoint.transform.position;
+            bullet.transform.rotation = bulletSpawnPoint.transform.rotation;
+            bullet.SetActive(true);
+            bullet.GetComponent<Rigidbody>().velocity  = turret.transform.forward * bulletSpeed;
+            stateController.resetAttackTimer();
+        }
+    }
+
+    private void deleteBulletInstance()
+    {
+        if(Vector3.Distance(turret.transform.position, bullet.transform.position) > tankData.maxAttackRange)
+        {
+            bullet.SetActive(false);
+        }
+    }
+
+    private bool lockedOn()
+    {
+        if(turret.transform.rotation == Quaternion.LookRotation((target.transform.position - turret.transform.position).normalized))
+        {
+            return true;
+        }
+        return false;
+    }
+}
