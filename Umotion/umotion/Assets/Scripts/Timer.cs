dc3b5dcd291557b27ab0e56b759e61134324ffc2
Thimo <tj.smits@outlook.com>
2019-11-02 17:47:38 +0100

    Added a timer to the example and workshop scene

---
+++ Umotion/umotion/Assets/Scripts/Timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{
+
+    public Text timerText;
+
+    private float time = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        timerText.text = time.ToString("F2");
+    }
+}
